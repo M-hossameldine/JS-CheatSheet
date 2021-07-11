628d8d13e4570b9a85f977974b45a6b344994774
Mohamed Hossam <m.hossameldine@gmail.com>
2021-07-11 19:06:51 +0200

    String Object & String Methods

---
+++ f.3.3-String_Object.js
@@ -0,0 +1,9 @@
+/*
+ - String.fromCharCode()
+*/
+
+// String.fromCharCode()
+/* Method returns a string created from the specified sequence of UTF-16 code units 
+  Continue from this link: 
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+*/
